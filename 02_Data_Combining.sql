ab6b715821fbafe952e36017dfcbf26808e28bf1
jena-harper <jen@chezharper.com>
2025-04-11 15:48:17 -0600

    Update 02_Data_Combining.sql

--- 02_Data_Combining.sql
+++ 02_Data_Combining.sql
@@ -1,1 +1,22 @@
-
+SELECT *, '2024_apr' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_apr`
+UNION ALL
+SELECT *, '2024_aug' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_aug`
+UNION ALL
+SELECT *, '2024_dec' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_dec`
+UNION ALL
+SELECT *, '2024_jul' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_jul`
+SELECT *, '2025_jan' AS source_month FROM `bike-share-user-analysis.bike_trips.2025_jan`
+UNION ALL
+SELECT *, '2025_feb' AS source_month FROM `bike-share-user-analysis.bike_trips.2025_feb`
+UNION ALL
+SELECT *, '2024_mar' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_mar`
+UNION ALL
+SELECT *, '2024_may' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_may`
+UNION ALL
+SELECT *, '2024_jun' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_jun`
+UNION ALL
+SELECT *, '2024_sept' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_sept`
+UNION ALL
+SELECT *, '2024_oct' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_oct`
+UNION ALL
+SELECT *, '2024_nov' AS source_month FROM `bike-share-user-analysis.bike_trips.2024_nov`
